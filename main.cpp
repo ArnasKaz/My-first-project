3dc8104ae754769e34355e42ed923cf048def8b4
ArnasKaz <arnas.kazanavicius@mif.stud.vu.lt>
2024-09-27 15:03:14 +0300

    v0.1
    Pridėta:
    1. Galimybė nuskaityti duomenis iš failo
    2. kursiokai.txt failas su nuskaitomais duomenimis

--- main.cpp
+++ main.cpp
@@ -1,42 +1,55 @@
 #include "biblioteka.h"
 #include "stud.h"
 
-int main()
-{
+int main() {
     vector<Stud> Vec1;
     Stud Temp;
     int n;
     char pasirinkimas;
 
-    cout << "Kiek is viso studentu? ";
-    cin >> n;
+    cout << "Ar norite duomenis irasyti ranka ar nuskaityti is failo? (r/f): ";
+    cin >> pasirinkimas;
 
+    if (pasirinkimas == 'f' || pasirinkimas == 'F') {
+        string failo_pavadinimas;
+        cout << "Irasykite failo pavadinima ";
+        cin >> failo_pavadinimas;
+
+        if (!skaitytiIsFailo(failo_pavadinimas, Vec1)) {
+            return 1;
+        }
+    } else {
+        cout << "Kiek is viso studentu? ";
+        cin >> n;
+
+        for (int i = 0; i < n; i++) {
+            cout << "Iveskite studento duomenis" << endl;
+            ived(Temp);
+            Vec1.push_back(Temp);
+        }
+    }
 
     cout << "Naudoti vidurki (v) ar mediana (m) galutinio balo skaiciavimui? (v/m): ";
     cin >> pasirinkimas;
 
-    for (int i = 0; i < n; i++) {
-        cout << "Iveskite studento duomenis" << endl;
-        ived(Temp);
 
-
+    for (int i = 0; i < Vec1.size(); i++) {
         if (pasirinkimas == 'm' || pasirinkimas == 'M') {
-            val(Temp, true);
+            val(Vec1[i], true);
         } else {
-            val(Temp, false);
+            val(Vec1[i], false);
         }
-
-        Vec1.push_back(Temp);
     }
 
-
+    // Antraštės
     cout << setw(15) << left << "Studento vardas: "
          << setw(10) << left << "Pavarde: "
          << setw(15) << right << "Galutinis ivertinimas: " << endl;
 
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < Vec1.size(); i++)
         output(Vec1.at(i));
 
     return 0;
 }
+
--- stud.cpp
+++ stud.cpp
@@ -1,6 +1,8 @@
 #include "stud.h"
 #include <algorithm>
 #include <random>
+#include <fstream>
+#include <sstream>
 
 void ived(Stud &Lok) {
     cout << "Iveskite studento varda: ";
@@ -9,7 +11,7 @@ void ived(Stud &Lok) {
     cout << "Iveskite studento pavarde: ";
     cin >> Lok.pavarde;
 
-    string pasirinkimas;
+    std::string pasirinkimas;
     cout << "Ar generuoti studento ivertinimus atsitiktinai? (taip/ne): ";
     cin >> pasirinkimas;
 
@@ -19,12 +21,12 @@ void ived(Stud &Lok) {
     int count = 0;
 
     if (pasirinkimas == "taip") {
-
         std::random_device rd;
         std::mt19937 gen(rd());
         std::uniform_real_distribution<> dist(1, 10);
-        std::uniform_int_distribution<> nd_count_dist(3, 10);
+        std::uniform_int_distribution<> nd_count_dist(1, 10);
         int m = nd_count_dist(gen);
+
         for (int i = 0; i < m; i++) {
             grade = dist(gen);
             Lok.ND.push_back(grade);
@@ -32,19 +34,14 @@ void ived(Stud &Lok) {
             count++;
         }
 
-
         Lok.vid = sum / count;
-
-
         Lok.egz = dist(gen);
         cout << "Sugeneruota " << m << " Namu darbu ivertinimai ir egzamino ivertinimas" << endl;
-
     } else if (pasirinkimas == "ne") {
         string input;
 
-
         while (true) {
-            cout << "Irasykite namu darbu ivertinimu, kol baigsite, tuomet rasykite 'baigti' kad baigtumete: ";
+            cout << "Irasykite namu darbu ivertinima, arba irasykite 'baigti', kad baigtumete ";
             cin >> input;
 
             if (input == "baigti") {
@@ -61,7 +58,6 @@ void ived(Stud &Lok) {
             }
         }
 
-
         if (count > 0) {
             Lok.vid = sum / count;
         } else {
@@ -70,25 +66,74 @@ void ived(Stud &Lok) {
 
         cout << "Irasykite egzamino ivertinima: ";
         cin >> Lok.egz;
-    } else {
-        cout << "Netinkamas pasirinkimas. Iveskite 'taip' arba 'ne'." << endl;
     }
 }
 
 
+bool skaitytiIsFailo(const std::string& failo_pavadinimas, std::vector<Stud>& studentai) {
+    std::ifstream failas(failo_pavadinimas);
+    if (!failas.is_open()) {
+        cout << "Nepavyko atidaryti failo " << failo_pavadinimas << endl;
+        return false;
+    }
+
+    std::string eilute;
+    std::getline(failas, eilute);
+
+    while (std::getline(failas, eilute)) {
+        std::istringstream iss(eilute);
+        Stud studentas;
+        iss >> studentas.vardas >> studentas.pavarde;
+
+        double pazymys;
+        for (int i = 0; i < 5; ++i) {
+            iss >> pazymys;
+            studentas.ND.push_back(pazymys);
+        }
+
+        iss >> studentas.egz;
+        studentai.push_back(studentas);
+
+
+        cout << "Namų darbai: ";
+        for (double nd : studentas.ND) {
+            cout << nd << " ";
+        }
+        cout << endl;
+
+        cout << "Egzamino pazymys: " << studentas.egz << endl;
+    }
+
+    failas.close();
+    return true;
+}
+
 void val(Stud &Lok, bool naudotiMediana) {
+    double sum = 0.0;
+    for (double nd : Lok.ND) {
+        sum += nd;
+    }
+
+    if (Lok.ND.size() > 0) {
+        Lok.vid = sum / Lok.ND.size();
+    } else {
+        Lok.vid = 0;
+    }
+
     if (naudotiMediana) {
         Lok.med = skaiciuotiMediana(Lok.ND);
         Lok.rez = 0.4 * Lok.med + 0.6 * Lok.egz;
     } else {
         Lok.rez = 0.4 * Lok.vid + 0.6 * Lok.egz;
     }
+
+
+    cout << "Galutinis rezultatas (vidurkis): " << Lok.rez << endl;
 }
 
 
 double skaiciuotiMediana(std::vector<double> &ND) {
     std::sort(ND.begin(), ND.end());
-
     size_t size = ND.size();
     if (size % 2 == 0) {
         return (ND[size / 2 - 1] + ND[size / 2]) / 2.0;
@@ -96,7 +141,6 @@ double skaiciuotiMediana(std::vector<double> &ND) {
         return ND[size / 2];
     }
 }
-
 
 void output(const Stud &Lok) {
     cout << setw(15) << left << Lok.vardas
